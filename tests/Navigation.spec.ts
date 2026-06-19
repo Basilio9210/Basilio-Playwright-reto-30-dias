223492fa0240c9e3c26637701a423794a59b9d51
Basilio9210 <basilio9210@gmail.com>
2026-06-18 19:54:06 -0500

    reto #4

--- tests/Navigation.spec.ts
+++ tests/Navigation.spec.ts
@@ -18,7 +18,7 @@ test('Check Menu Options', async ({ page }) => {
 
     // Validación manual del primer ítem del menú
     const firstItemText = await leftMenuItems.first().innerText();
-    if (firstItemText.trim() === 'Admin1') {
+    if (firstItemText.trim() === 'Admin') {
         console.log('✅ Admin is the expected first menu item.');
     } else {
         console.error(`❌ Error: The first item is "${firstItemText}", not the expected "Admin"`);
@@ -32,7 +32,9 @@ test('Check Menu Options', async ({ page }) => {
     for (let i = 0; i < CurrentMenuItemsCount; i++) {
 
         const menuItmenuText = await leftMenuItems.nth(i).innerText();
-        currentMenuItems.push(menuItmenuText);
+        // normalize text: take first line and trim to avoid newlines/extra spaces
+        const normalized = menuItmenuText.split('\n')[0].trim();
+        currentMenuItems.push(normalized);
     }
     console.log('Menu Items:', currentMenuItems);
 
@@ -49,11 +51,44 @@ test('Check Menu Options', async ({ page }) => {
         'Dashboard',
         'Directory',
         'Maintenance',
-        'Claim',
         'Buzz'];
 
     expect(currentMenuItems).toEqual(expectedMenuItems)
 });
 
+test('Navigate left Menu', async ({ page }) => {
+
+    // Navegar a la página de inicio de sesión
+    await page.goto('https://opensource-demo.orangehrmlive.com');
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Validar que el enlace "Admin" esté visible después de iniciar sesión
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+    //Encuentra los elementos del menú lateral y cuenta cuántos hay
+    const leftMenuItems = page.getByLabel('Sidepanel').getByRole('listitem')
+    const CurrentMenuItemsCount = await leftMenuItems.count();
+
+    for (let i = 0; i < CurrentMenuItemsCount; i++) {
+
+        const menuItem = leftMenuItems.nth(i);
+        const menuItemText = await menuItem.innerText();
+
+        console.log(`Clicking on menu item: ${menuItemText}`);
+
+        if (menuItemText !== 'Maintenance') {
+
+            await menuItem.click();
+
+        }
+        else {
+            await page.goBack();
+        }
+
+    }
+}
 
 
+);

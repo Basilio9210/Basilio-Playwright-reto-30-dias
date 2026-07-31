8f8fccd466a567eb5376857dca48cba6b41c2278
Basilio9210 <basilio9210@gmail.com>
2026-07-31 17:20:23 -0500

    Change in DEV

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,7 +7,7 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test('get started link but only in Dev', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Click the get started link.

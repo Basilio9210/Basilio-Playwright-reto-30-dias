c941bd22e5d8b67d3917be692a22ef476d7abb57
Basilio9210 <basilio9210@gmail.com>
2026-08-22 13:31:30 -0500

    Día 28: Agregando un usuario admin usando el API

--- tests/users.spec.ts
+++ tests/users.spec.ts
@@ -35,6 +35,65 @@ test ('API Test Get All the Users', async ({page, request})=>{
 
 } )
 
+
+test ('API Test Add a new User', async ({page, request})=>{
+
+    const authFilePath = path.resolve(process.cwd(), '.auth', 'admin.json')
+    const authState = JSON.parse(await readFile(authFilePath, 'utf-8')) as {
+        cookies?: Array <{name: string, value: string}>
+    }
+    const orangeHrmCookie = authState.cookies?.find(cookie => cookie.name = 'orangehrm')
+    expect (orangeHrmCookie, 'The orangehrm cookie was not found in the saved auth state').toBeTruthy()
+
+    const username = 'user' + crypto.randomUUID().slice(0, 20)
+    const password = 'admin12345'
+
+    const cookieHeader =  `orangehrm=${orangeHrmCookie?.value}`
+    const response = await request.post('https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/admin/users', {
+        headers: {
+            Cookie: cookieHeader,
+            Accept: 'application/json'
+        },
+        data: {
+    "username": username,
+    "password": password,
+    "status": true,
+    "userRoleId": 1,
+    "empNumber": 116
+}
+    })
+
+    expect (response.ok()).toBeTruthy()
+    const bodyJson = await  response.json()
+    console.log(JSON.stringify(await bodyJson))
+
+
+} )
+
+test ('API Test Get All the Users', async ({page, request})=>{
+
+    const authFilePath = path.resolve(process.cwd(), '.auth', 'admin.json')
+    const authState = JSON.parse(await readFile(authFilePath, 'utf-8')) as {
+        cookies?: Array <{name: string, value: string}>
+    }
+    const orangeHrmCookie = authState.cookies?.find(cookie => cookie.name = 'orangehrm')
+    expect (orangeHrmCookie, 'The orangehrm cookie was not found in the saved auth state').toBeTruthy()
+
+    const cookieHeader =  `orangehrm=${orangeHrmCookie?.value}`
+    const response = await request.get('https://opensource-demo.orangehrmlive.com/web/index.php/api/v2/admin/users?limit=50&offset=0&sortField=u.userName&sortOrder=ASC', {
+        headers: {
+            Cookie: cookieHeader,
+            Accept: 'application/json'
+        }
+    }) 
+
+    expect (response.ok()).toBeTruthy()
+    const bodyJson = await  response.json()
+    console.log(JSON.stringify(await bodyJson))
+
+
+} )
+
 test('Get all the usernames registered', async ({ page }) => {
 
     const loginPage = new LoginPage(page)
